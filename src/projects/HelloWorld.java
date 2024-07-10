6b1841fde9d67e581f20df394ba22f448726effa
Lemi02 <m.vanderleest@roc-nijmegen.nl>
2024-07-10 16:00:07 +0200

    Code is geformateerd Ziet er nu beter uit

--- src/projects/HelloWorld.java
+++ src/projects/HelloWorld.java
@@ -8,8 +8,8 @@ public class HelloWorld
 	// TODO Auto-generated method stub
 	System.out.println("Hello World!");
 	System.out.println("Oefenen met GitHub");
-System.out.println("Snap ik dit dan eindelijk?");
-System.out.println("Dat zou wel tijd worden");
+	System.out.println("Snap ik dit dan eindelijk?");
+	System.out.println("Dat zou wel tijd worden");
     }
 
 }
